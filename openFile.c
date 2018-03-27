d654bde679ad89509797c9b6f6ef83f97bb8a44f
lms251 <31562399+lms251@users.noreply.github.com>
2018-03-27 18:54:47 -0500

    Updated open function
    includes the text storage variable that stores the contents of the screen into a 2D array

--- openFile.c
+++ openFile.c
@@ -1,20 +1,51 @@
 #include <ncurses.h>
+#include <string.h>
+#include <stdlib.h>
 
 //ncurses should already be open with a new window.
+int columns = 80;
+int rows = 25;
 
-int openFile(char fileName[]) {
+int openFile(char fileName[], char text[columns][rows]) {
 	FILE *fp;
 	if(!(fp = fopen (fileName, "r"))){
 		printw("File not found");
 		refresh();
 		return -1;
 	}
-	char buff[255];
 	char temp;
-	while(fscanf(fp, "%s", buff) != EOF){
-		temp = fgetc(fp);
-		printw("%s%c", buff, temp);		
-		refresh();
+	int i = 0; //columns
+	int j = 0; //rows
+	while((temp = fgetc(fp)) != EOF)
+	{
+		if(i == columns){
+			text[i][j] = '\n';
+			j++;
+		}
+		if(j == rows){
+			printw("File too large, cannot open to edit");
+			refresh();
+			return -1;
+		}
+		text[i][j] =  temp;
+		text[i+1][j] = '\0';
+		i++;
+	}
+
+	bool bre = false;	
+	for(int k = 0; k < rows; k++)
+	{
+		for(int g = 0; g < columns; g++) {
+			printw("%c", text[g][k]);
+			refresh();
+
+			if (text[g][k] == '\0') {
+				bre = true;
+				break;
+			}
+		}	
+		if( bre) 
+			break;
 	}
 
 	return 0;
@@ -23,8 +54,9 @@ int openFile(char fileName[]) {
 
 int main(int argc, char*argv[])
 {
+	char text[columns][rows];
 	initscr();
-	openFile(argv[1]);
+	openFile(argv[1], text);
 	getch();
 	endwin();
 
