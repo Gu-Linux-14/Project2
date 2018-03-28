c1c17d658da4e09f645a36c92a34e3b1f0415012
lms251 <31562399+lms251@users.noreply.github.com>
2018-03-28 17:18:48 -0500

    my new files with mains
    in case anyone needs them?

---
+++ openFileMain.c
@@ -0,0 +1,79 @@
+#include <ncurses.h>
+#include <string.h>
+#include <stdlib.h>
+
+//ncurses should already be open with a new window.
+int columns = 80;
+int rows = 25;
+
+int openFile(char fileName[], char text[rows][columns]) {
+	FILE *fp;
+	if(!(fp = fopen (fileName, "r"))){
+		printw("File not found");
+		refresh();
+		return -1;
+	}
+	char temp;
+	int i = 0; //columns
+	int j = 0; //rows
+	while((temp = fgetc(fp)) != EOF)
+	{
+		if(i == columns - 1){
+			text[j][i] = '\n';
+			j++;
+			i = 0;
+		}
+		if(j == rows - 1){
+			printw("File too large, cannot open to edit");
+			refresh();
+			return -1;
+		}
+		text[j][i] =  temp;
+		text[j][i+1] = '\0';
+		i++;
+	}
+
+	bool bre = false;	
+	for(int k = 0; k < rows; k++)
+	{
+		for(int g = 0; g < columns; g++) {
+			printw("%c", text[k][g]);
+			refresh();
+
+			if (text[k][g] == '\0') {
+				bre = true;
+				break;
+			}
+		}	
+		if( bre) 
+			break;
+	}
+	
+	fclose(fp);
+	return 0;
+	
+} 
+
+int main(int argc, char*argv[])
+{
+	char text[rows][columns];
+	initscr();
+	openFile(argv[1], text);
+	getch();
+	endwin();
+/*	bool bre = false;
+	for(int k = 0; k < rows; k++) {
+		for (int g = 0; g< columns; g++) {
+			printf("%c", text[g][k]);
+			if(text[g][k] == '\0') {
+				bre = true;
+				break;
+			}
+		}	
+		if (bre)
+			break;
+	}
+*/	
+	return 0;
+
+}
---
+++ saveFileMain.c
@@ -0,0 +1,42 @@
+#include <ncurses.h>
+#include <stdlib.h>
+
+int columns = 80;
+int rows = 25;
+
+int saveFile (char fileName[], char text[rows][columns]) {
+	FILE *ofp;
+	if(!(ofp = fopen (fileName, "w"))) {
+		return -1;
+	}
+	
+	bool bre = false; //whether or not the file is done
+	for(int k = 0; k < rows; k++) {
+		for(int g = 0; g < columns; g++){
+			if(text[k][g] == '\0') {
+				bre = true;
+				break;
+			}
+			fputc(text[k][g], ofp);
+			if(text[k][g] == '\n')
+				break;
+		}
+		if (bre)
+			break;
+	}
+	fclose(ofp);
+	return 0;
+}
+
+int main(int argc, char*argv[]) {
+	char text[rows][columns];
+	initscr();
+	text[0][0] = 'a';
+	text[0][1] = '\n';
+	text[1][0] = 'b';
+	text[1][1] = 'c';
+	text[1][2] = '\0';
+	saveFile(argv[1], text);
+	getch();
+	endwin();
+}
