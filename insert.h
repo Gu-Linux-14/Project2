c4bc3b68c0ce362b820f0d5c60e48cf46a838c4f
Elijah Garcia <e_g257@txstate.edu>
2018-03-29 20:11:06 +0000

    Create insert.h

---
+++ insert.h
@@ -0,0 +1,11 @@
+#ifndef __prjt2_14__insert__
+#define __prjt2_14__insert__
+
+#include <ncurses.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <openFile.h>
+#include <editText.h>
+
+int insert();
