bc34a2d5afefbb1c7d215a4f15985db2f2695f99
carterkindred33ck <C_k133@txstate.edu>
2018-03-29 16:10:15 -0500

    Add files via upload

---
+++ copy.c
@@ -0,0 +1,27 @@
+#include <ncurses.h>
+
+
+
+int copy(WINDOW *stdscr)
+{
+	int userInput, x0, y0, x1, y1; 
+
+
+	FILE *destinationWindow; 
+
+	textwin = newwin()
+
+	wprint("Enter copy text prompt"); 
+	userInput = getch(); 
+
+	for(int i = 0; i < size; i++)
+	{
+
+
+
+
+
+	}
+
+
+}
---
+++ copy.h
@@ -0,0 +1,9 @@
+#ifndef __prjt2_14__copy__
+#define __prjt2_14__copy__
+
+#include <ncurses.h>
+#inlcude "openFile.h"
+
+int copy(WINDOW *stdscr); 
+
+#endif
